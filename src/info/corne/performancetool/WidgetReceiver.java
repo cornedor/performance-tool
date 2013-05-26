d7a23c468a0de263295f496084b96e10914cfa55
maxwen <max.weninger@gmail.com>
2013-05-26 22:32:32 +0200

    removed tabs

--- src/info/corne/performancetool/WidgetReceiver.java
+++ src/info/corne/performancetool/WidgetReceiver.java
@@ -40,77 +40,77 @@ import android.widget.RemoteViews;
 
 public class WidgetReceiver extends AppWidgetProvider
 {
-	private static final String ACTION_CLICK = "ACTION_CLICK";
-	private static int current = 0;
-	@Override
-	public void onReceive(Context context, Intent intent)
-	{
-		// AppWidgetManager mgr = AppWidgetManager.getInstance(context);
-		if(intent.getAction().equals(ACTION_CLICK))
-		{
-			SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
-			RemoteViews rv = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
-			current =  sharedPreferences.getInt(Settings.CURRENT_WIDGET_PROFILE, 0);
-			if(current == 2)
-			{
-				rv.setImageViewResource(R.id.widgetButton, R.drawable.widget_audio);
-				current = 1;
+    private static final String ACTION_CLICK = "ACTION_CLICK";
+    private static int current = 0;
+    @Override
+    public void onReceive(Context context, Intent intent)
+    {
+        // AppWidgetManager mgr = AppWidgetManager.getInstance(context);
+        if(intent.getAction().equals(ACTION_CLICK))
+        {
+            SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+            RemoteViews rv = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
+            current =  sharedPreferences.getInt(Settings.CURRENT_WIDGET_PROFILE, 0);
+            if(current == 2)
+            {
+                rv.setImageViewResource(R.id.widgetButton, R.drawable.widget_audio);
+                current = 1;
                 AudioSettings settings = new AudioSettings();
                 String[] files = settings.getFileNames();
                 String[] values = settings.getValues();
 
-				SetHardwareInfoTask task = new SetHardwareInfoTask(files, values);
-				task.execute();
-			}
-			else if (current == 1)
-			{
-				rv.setImageViewResource(R.id.widgetButton, R.drawable.widget_power);
-				current = 2;
+                SetHardwareInfoTask task = new SetHardwareInfoTask(files, values);
+                task.execute();
+            }
+            else if (current == 1)
+            {
+                rv.setImageViewResource(R.id.widgetButton, R.drawable.widget_power);
+                current = 2;
                 PowerSettings settings = new PowerSettings();
                 String[] files = settings.getFileNames();
                 String[] values = settings.getValues();
 
-				SetHardwareInfoTask task = new SetHardwareInfoTask(files, values);
-				task.execute();
-			}
-			else if (current == 0)
-			{
-				rv.setImageViewResource(R.id.widgetButton, R.drawable.widget_default);
-				current = 0;
+                SetHardwareInfoTask task = new SetHardwareInfoTask(files, values);
+                task.execute();
+            }
+            else if (current == 0)
+            {
+                rv.setImageViewResource(R.id.widgetButton, R.drawable.widget_default);
+                current = 0;
                 DefaultSettings settings = new DefaultSettings();
                 String[] files = settings.getFileNames();
                 String[] values = settings.getValues();
 
-				SetHardwareInfoTask task = new SetHardwareInfoTask(files, values);
-				task.execute();
-			} else {
-			}
-			
-			Editor editor = sharedPreferences.edit();
-			editor.putInt(Settings.CURRENT_WIDGET_PROFILE, current);
-			editor.commit();
-			ComponentName cn = new ComponentName(context, WidgetReceiver.class);
-			(AppWidgetManager.getInstance(context)).updateAppWidget(cn, rv);
-		}
-		super.onReceive(context, intent);
-	}
-	@Override
-	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds)
-	{
-		for(int i = 0; i < appWidgetIds.length; ++i) {
-			RemoteViews rv = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
-			
-			Intent clickIntent = new Intent(context, WidgetReceiver.class);
-			
-			clickIntent.setAction(ACTION_CLICK);
-			clickIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetIds[i]);
-			clickIntent.setData(Uri.parse(clickIntent.toUri(Intent.URI_INTENT_SCHEME)));
-			PendingIntent clickPendingIntent = PendingIntent.getBroadcast(
-					context, 0, clickIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-			rv.setOnClickPendingIntent(R.id.widgetButton, clickPendingIntent);
-			appWidgetManager.updateAppWidget(appWidgetIds[i], rv);
-			
-		}
-		super.onUpdate(context, appWidgetManager, appWidgetIds);
-	}
+                SetHardwareInfoTask task = new SetHardwareInfoTask(files, values);
+                task.execute();
+            } else {
+            }
+            
+            Editor editor = sharedPreferences.edit();
+            editor.putInt(Settings.CURRENT_WIDGET_PROFILE, current);
+            editor.commit();
+            ComponentName cn = new ComponentName(context, WidgetReceiver.class);
+            (AppWidgetManager.getInstance(context)).updateAppWidget(cn, rv);
+        }
+        super.onReceive(context, intent);
+    }
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds)
+    {
+        for(int i = 0; i < appWidgetIds.length; ++i) {
+            RemoteViews rv = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
+            
+            Intent clickIntent = new Intent(context, WidgetReceiver.class);
+            
+            clickIntent.setAction(ACTION_CLICK);
+            clickIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetIds[i]);
+            clickIntent.setData(Uri.parse(clickIntent.toUri(Intent.URI_INTENT_SCHEME)));
+            PendingIntent clickPendingIntent = PendingIntent.getBroadcast(
+                    context, 0, clickIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+            rv.setOnClickPendingIntent(R.id.widgetButton, clickPendingIntent);
+            appWidgetManager.updateAppWidget(appWidgetIds[i], rv);
+            
+        }
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
 }
